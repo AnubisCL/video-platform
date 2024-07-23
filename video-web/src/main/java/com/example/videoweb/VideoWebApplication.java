8dc8bcbccadf756f37cd4a27d44b5a0f48e84bd8
chailei <anubiscl@163.com>
2024-07-23 13:48:48 +0800

    feat(video-web):init

---
+++ video-web/src/main/java/com/example/videoweb/VideoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.videoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoWebApplication.class, args);
+    }
+
+}
