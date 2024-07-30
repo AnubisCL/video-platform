3e5b30e7b8198a101317cdded4dca10af1bd1e0a
chailei <anubiscl@163.com>
2024-07-31 01:46:25 +0800

    feat(video-web):task[V1.0]

--- video-web/src/main/java/com/example/videoweb/controller/TaskController.java
+++ video-web/src/main/java/com/example/videoweb/controller/TaskController.java
@@ -2,11 +2,17 @@ package com.example.videoweb.controller;
 
 import com.example.videoweb.domain.vo.ResultVo;
 import com.example.videoweb.service.ITaskService;
-import io.swagger.v3.oas.annotations.Parameter;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * <p>
@@ -22,10 +28,26 @@ public class TaskController {
 
     @Resource private ITaskService taskService;
 
-    @PostMapping("/insertTaskJson")
-    public ResultVo insertTaskJson(@Parameter(description = "导入json") String json) {
-        //todo：导入json到task表
-        return ResultVo.ok("导入成功");
+    @PostMapping("/insertTaskTxt")
+    public ResultVo insertTaskTxt(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ResultVo.error("文件未上传");
+        }
+        try {
+            // 获取文件名
+            String fileName = file.getOriginalFilename();
+            // 指定文件保存目录
+            //"/Users/anubis/IdeaProjects/MyGitHub/video-plaform"
+            Path uploadPath = Paths.get(System.getProperty("user.dir"));
+            // 将文件保存到指定目录
+            Files.copy(file.getInputStream(), uploadPath.resolve(fileName));
+            // todo: 导入txt到task表
+            // 这里可以添加解析文件并将数据导入数据库的逻辑
+
+            return ResultVo.ok("文件上传成功");
+        } catch (IOException e) {
+            return ResultVo.error("文件上传失败：" + e.getMessage());
+        }
     }
 
 }
