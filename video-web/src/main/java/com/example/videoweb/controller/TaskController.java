136ed68a81156b55cb6abfa9889ee247fb77ddff
chailei <anubiscl@163.com>
2024-07-31 09:56:04 +0800

    feat(video-web):task[V1.1]

--- video-web/src/main/java/com/example/videoweb/controller/TaskController.java
+++ video-web/src/main/java/com/example/videoweb/controller/TaskController.java
@@ -1,18 +1,23 @@
 package com.example.videoweb.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.example.videoweb.domain.entity.Task;
+import com.example.videoweb.domain.enums.StatusEnum;
+import com.example.videoweb.domain.enums.TaskStatusEnum;
 import com.example.videoweb.domain.vo.ResultVo;
 import com.example.videoweb.service.ITaskService;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
+import java.util.*;
 
 /**
  * <p>
@@ -22,6 +27,7 @@ import java.nio.file.Paths;
  * @author anubis
  * @since 2024-07-30
  */
+@Slf4j
 @RestController
 @RequestMapping("/task/")
 public class TaskController {
@@ -33,17 +39,34 @@ public class TaskController {
         if (file.isEmpty()) {
             return ResultVo.error("文件未上传");
         }
+        List<Task> taskList = new ArrayList<>();
         try {
-            // 获取文件名
-            String fileName = file.getOriginalFilename();
-            // 指定文件保存目录
-            //"/Users/anubis/IdeaProjects/MyGitHub/video-plaform"
-            Path uploadPath = Paths.get(System.getProperty("user.dir"));
-            // 将文件保存到指定目录
-            Files.copy(file.getInputStream(), uploadPath.resolve(fileName));
-            // todo: 导入txt到task表
-            // 这里可以添加解析文件并将数据导入数据库的逻辑
-
+            BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] split = line.split("\\|");
+                if (split.length == 4) {
+                    String title = split[0];
+                    String infoUrl = split[1];
+                    String imageUrl = split[2];
+                    String m3u8Url = split[3];
+                    Task task = new Task();
+                    Map<String, String> jsonMap = new HashMap<>();
+                    jsonMap.put("infoUrl", infoUrl);
+                    jsonMap.put("imageUrl", imageUrl);
+                    task.setTaskName(title);
+                    task.setDownloadUrl(m3u8Url);
+                    task.setTaskType("m3u8");
+                    task.setDownloadJson(JSON.toJSONString(jsonMap));
+                    task.setStatus(StatusEnum.YES.getStatus());
+                    task.setTaskStatus(TaskStatusEnum.UN_START.getCode());
+                    taskList.add(task);
+                    log.info("任务：{}", taskList.size());
+                } else {
+                    log.info("任务：{} - {}", taskList.size(), line);
+                }
+            }
+            taskService.saveBatch(taskList);
             return ResultVo.ok("文件上传成功");
         } catch (IOException e) {
             return ResultVo.error("文件上传失败：" + e.getMessage());
